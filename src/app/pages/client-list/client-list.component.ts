f2d095023bc29291b781468c863631889f570c99
Anish Kumar Moningi <anish.moningi@RSK-BSL.COM>
2022-01-13 18:06:56 +0530

    integrated logo in login page

--- src/app/pages/client-list/client-list.component.ts
+++ src/app/pages/client-list/client-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MockService } from 'src/app/service/mock.service';
@@ -18,24 +18,35 @@ export class ClientListComponent implements OnInit {
   pageSize = 10;
   pageIndex = 0;
   pageSizeOptions = [5, 10, 25];
-  colorControl = new FormControl('one');
-  clientForm:ClientForm[]=[
-    {key:'Active',formName:'Active'},
-    {key:'Solarwinds',formName:'Any'},
-    {key:'IASO',formName:'Any'},
-    {key:'Mail',formName:'Any'},
-    {key:'CEL',formName:'Any'},
-    {key:'CST',formName:'Any'},
-    {key:'Site Contact',formName:'Any'},
-    {key:'Roadmaps',formName:'Any'},
-    {key:'teams',formName:'Any'},
+  clientListForm:any
+  // colorControl = new FormControl('one');
+  clientData:ClientForm[]=[
+    {key:'Active',formName:'active'},
+    {key:'Solarwinds',formName:'solarwinds'},
+    {key:'IASO',formName:'IASO'},
+    {key:'Mail',formName:'Mail'},
+    {key:'CEL',formName:'CEL'},
+    {key:'CST',formName:'CST'},
+    {key:'Site Contact',formName:'site_contact'},
+    {key:'Roadmaps',formName:'roadmaps'},
+    {key:'teams',formName:'teams'},
   ]
-  recentConversation:string[]=['Name','Primary_Site_Contact','CST','Solarwinds','IASO','Mail_Scanning','CEL','Active','Team','Reveiw_Date']
-  // dataSource = this.mock.userData;
+  clientList:string[]=['Name','Primary_Site_Contact','CST','Solarwinds','IASO','Mail_Scanning','CEL','Active','Team','Reveiw_Date']
   dataSource = new MatTableDataSource(this.mock.userData);
-  constructor(private mock:MockService) { }
+  constructor(private mock:MockService ,private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.clientListForm=this.formBuilder.group({
+      active:['one'],
+      solarwinds:['one'],
+      IASO:['one'],
+      Mail:['one'],
+      CEL:['one'],
+      CST:['one'],
+      site_contact:['one'],
+      roadmaps:['one'],
+      teams:['one']
+    })    
   }
   ngAfterViewInit() {
     this.dataSource.paginator=this.paginator
