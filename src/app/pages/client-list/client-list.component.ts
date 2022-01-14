0ba7bb84c77e8fbba72d1d1ab97d6580c54b43fe
Anish Kumar Moningi <anish.moningi@RSK-BSL.COM>
2022-01-14 18:48:20 +0530

    update in client list

--- src/app/pages/client-list/client-list.component.ts
+++ src/app/pages/client-list/client-list.component.ts
@@ -6,6 +6,7 @@ import { MockService } from 'src/app/service/mock.service';
 export interface ClientForm{
   key:string;
   formName:string;
+  values:string[]
 }
 @Component({
   selector: 'app-client-list',
@@ -19,37 +20,42 @@ export class ClientListComponent implements OnInit {
   pageIndex = 0;
   pageSizeOptions = [5, 10, 25];
   clientListForm:any
-  // colorControl = new FormControl('one');
   clientData:ClientForm[]=[
-    {key:'Active',formName:'active'},
-    {key:'Solarwinds',formName:'solarwinds'},
-    {key:'IASO',formName:'IASO'},
-    {key:'Mail',formName:'Mail'},
-    {key:'CEL',formName:'CEL'},
-    {key:'CST',formName:'CST'},
-    {key:'Site Contact',formName:'site_contact'},
-    {key:'Roadmaps',formName:'roadmaps'},
-    {key:'teams',formName:'teams'},
+    {key:'Active',formName:'active',values:['Active','In Active']},
+    {key:'Solarwinds',formName:'solarwinds',values:['Has Solarwinds','No Solarwinds']},
+    {key:'IASO',formName:'IASO',values:['Has IASO','No IASO']},
+    {key:'Mail',formName:'Mail',values:['Has Mail','No Mail']},
+    {key:'CEL',formName:'CEL',values:['Has CEL','No CEL']},
+    {key:'CST',formName:'CST',values:['Has CST','No CST']},
+    {key:'Site Contact',formName:'site_contact',values:['Has Site Contact','No Site Contact']},
+    {key:'Roadmaps',formName:'roadmaps',values:['Has Roadmaps','No Roadmaps']},
+    {key:'teams',formName:'teams',values:['Atlantis','Nautilus','Watchtower','Apollo','Soteria']},
   ]
   clientList:string[]=['Name','Primary_Site_Contact','CST','Solarwinds','IASO','Mail_Scanning','CEL','Active','Team','Reveiw_Date']
-  dataSource = new MatTableDataSource(this.mock.userData);
+  dataSource = new MatTableDataSource(this.mock.userData.Clients.clients);
   constructor(private mock:MockService ,private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    // console.log('-=-==-=-=-=--=->',this.mock.userData.Clients.clients);
     this.clientListForm=this.formBuilder.group({
-      active:['one'],
-      solarwinds:['one'],
-      IASO:['one'],
-      Mail:['one'],
-      CEL:['one'],
-      CST:['one'],
-      site_contact:['one'],
-      roadmaps:['one'],
-      teams:['one']
-    })    
+      active:[''],
+      solarwinds:[''],
+      IASO:[''],
+      Mail:[''],
+      CEL:[''],
+      CST:[''],
+      site_contact:[''],
+      roadmaps:[''],
+      teams:['']
+    })   
+    console.log(this.clientListForm);
+     
   }
   ngAfterViewInit() {
-    this.dataSource.paginator=this.paginator
+    this.dataSource.paginator=this.paginator 
+  }
+  filter(event:Event){
+    this.dataSource.filter='Active'
   }
   // handlePageEvent(event: PageEvent) {
   //   this.length = event.length;
